800db6003e5c237b88186ae55e1d18987daa4b8d
swamhtettun <44191283+swamhtettun@users.noreply.github.com>
2018-12-25 15:11:38 +0800

    Delete urls.py

--- login_example/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.contrib.auth.views import LoginView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(), name='login'),
-]
